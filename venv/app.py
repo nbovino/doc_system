b6cca23486b843c857d8ac2020c1db3f58bae633
parse-tech <parse.technologies@gmail.com>
2020-01-07 21:32:52 -0500

    Base site

---
+++ venv/app.py
@@ -0,0 +1,23 @@
+from flask import (Flask, flash, render_template, redirect, url_for, request, session)
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask_bcrypt import generate_password_hash, check_password_hash
+from sqlalchemy import exc
+import pandas as pd
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+import datetime
+import forms
+import db_connect
+import models
+import re
+
+DEBUG = True
+PORT = 8000
+HOST = '0.0.0.0'
+
+app = Flask(__name__)
+app.secret_key = 'nfj2984ijNDUUFH89()&&iJINOkf)(_@KLNFE:!#RefwkFpyio'
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
