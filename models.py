2d429f4d9386c28647305a1ed84773348ad40172
nbovino <nbovino05@gmail.com>
2020-05-18 20:53:00 -0400

    Add User and EquipmentSignOut table. Add column to asset table.

--- models.py
+++ models.py
@@ -1,7 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey, Numeric, UnicodeText, TEXT, Boolean, JSON, DateTime
+from sqlalchemy.dialects import postgresql
 from sqlalchemy.dialects.postgresql import ARRAY
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_utils import PasswordType
+
+passlib = None
+try:
+    import passlib
+    from passlib.context import LazyCryptContext
+except ImportError:
+    pass
+
 from flask_login import UserMixin
 
 Base = declarative_base()
@@ -11,6 +21,14 @@ Base = declarative_base()
 # #     __tablename__ = "item_types"
 # #     id = Column(Integer, primary_key=True)
 # #     item_type = Column(String, unique=True)
+class Users(Base):
+    __tablename__ = 'Users'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    username = Column(String, unique=True)
+    password = Column(PasswordType(schemes=['pbkdf2_sha512']))
+
 
 class Assets(Base):
     __tablename__ = 'Assets'#Tells what table using
@@ -33,22 +51,31 @@ class Assets(Base):
     decommissioned = Column(DateTime)
 
 
+class EquipmentSignOut(Base):
+    __tablename__ = 'EquipmentSignOut'
+    id = Column(Integer, primary_key=True)
+    asset_id = Column(Integer, ForeignKey('Assets.id'))
+    check_out_date = Column(Date)
+    check_in_date = Column(Date)
+    user = Column(Integer, ForeignKey('Users.id'))
+
+
 class Departments(Base):
-    __tablename__ = "departments"
+    __tablename__ = "Departments"
     id = Column(Integer, primary_key=True)
     department = Column(String)
 
 
 class ItemModels(Base):
-    __tablename__ = "item_models"
+    __tablename__ = "ItemModels"
     id = Column(Integer, primary_key=True)
-    item_model = Column(String, unique=True)
-    manufacturer = Column(Integer, ForeignKey("manufacturers.id"))
+    item_model = Column(String)
+    manufacturer = Column(Integer, ForeignKey("Manufacturers.id"))
     item_desc = Column(TEXT)
 
 
 class Manufacturers(Base):
-    __tablename__ = 'manufacturers'
+    __tablename__ = 'Manufacturers'
     id = Column(Integer, primary_key=True)
     manufacturer = Column(String, unique=True)
 
@@ -66,7 +93,6 @@ class Software(Base):
     software_version = Column(String)
 
 
-#TODO: Make another column for primary asset type. Mainly to show when searching to associate solutions
 class Solutions(Base):
     __tablename__ = 'Solutions'  #Tells what table using
     id = Column(Integer, primary_key=True)
@@ -80,3 +106,4 @@ class Solutions(Base):
     primary_asset_type = Column(Integer)
     associated_asset_types = Column(ARRAY(Integer, dimensions=1))
     associated_software = Column(ARRAY(Integer, dimensions=1))
+    public = Column(Boolean, default=False)
